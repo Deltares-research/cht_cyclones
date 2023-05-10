b3d1cb28fb72776f98fd97db8aea740d6f9298dd
GundulaW <gundulawinter@gmail.com>
2023-05-10 14:43:07 +0200

    test to load database

--- tests/test_track.py
+++ tests/test_track.py
@@ -1,2 +1,13 @@
-def test_arithmetic():
-    assert 1 + 1 == 2
+import os
+from pathlib import Path
+
+from cht_cyclones.cyclone_track_database import CycloneTrackDatabase
+
+
+def test_load_track_database():
+    database_file = Path(os.getcwd()).joinpath(
+        "cyclone_track_database", "IBTrACS.NA.v04r00.nc"
+    )
+    tc = CycloneTrackDatabase("ibtracs", file_name=database_file)
+
+    assert tc.nstorms == 2323

0d42f299e7c0bee1a47da78f983fe187c019cf3c
Luuk Blom <Luuk.Blom@deltares.nl>
2025-01-22 11:33:18 +0100

    reformat tests so pytest can discover them

--- tests/test_forecasting.py
+++ tests/test_forecasting.py
@@ -6,39 +6,43 @@ from pathlib import Path
 from cht_cyclones.cyclone_track_database import CycloneTrackDatabase
 from cht_cyclones.tropical_cyclone import TropicalCyclone, TropicalCycloneEnsemble
 
-# Create track file
-database_file = Path(__file__).parent / "IBTrACS.ALL.v04r00.nc"
-db = CycloneTrackDatabase("ibtracs", file_name=database_file)
-ind = db.list_names().index("IDAI")
-tc = db.get_track(index=ind)
-tc.write_track(Path(__file__).parent / "best_track_idai.cyc", "ddb_cyc")
 
-# Define folder and track
-tc = TropicalCyclone(name="Idai")
-current_directory = os.path.dirname(os.path.abspath(__file__))
+def test_forecasting():
+    # Create track file
+    database_file = Path(__file__).parent / "IBTrACS.ALL.v04r00.nc"
+    db = CycloneTrackDatabase("ibtracs", file_name=database_file)
+    ind = db.list_names().index("IDAI")
+    tc = db.get_track(index=ind)
+    tc.write_track(Path(__file__).parent / "best_track_idai.cyc", "ddb_cyc")
 
-# Read a track
-file_path = os.path.join(current_directory, "best_track_idai.cyc")
-tc.from_ddb_cyc(file_path)
-tc.account_for_forward_speed()
-tc.estimate_missing_values()
-tc.include_rainfall = True
+    # Define folder and track
+    tc = TropicalCyclone(name="Idai")
+    current_directory = os.path.dirname(os.path.abspath(__file__))
 
-# Forecasting
-# Uses DeMaria et al. 2009 formulations
-tc2 = TropicalCycloneEnsemble(name="Idai_forecastng", TropicalCyclone=tc)
-tc2.dt = 3  # 3 hourly
-tc2.include_best_track = 0  # not included best-track
-tc2.tstart = datetime(2019, 3, 9, 0, 0, 0)  # March 5, 2019 at midnight UTC
-tc2.tend = datetime(2019, 3, 16, 6, 0, 0)  # last timestamp in cyc
-nm_to_m = float(1.852) * 1000  # nm to m
-tc2.mean_abs_cte24 = (
-    19.0397 * nm_to_m
-)  # mean absolute error in cross-track error (CTE) in meter
-tc2.sc_cte = 1.3253  # auto-regression CTE; typically >1
-tc2.compute_ensemble(10)
+    # Read a track
+    file_path = os.path.join(current_directory, "best_track_idai.cyc")
+    tc.from_ddb_cyc(file_path)
+    tc.account_for_forward_speed()
+    tc.estimate_missing_values()
+    tc.include_rainfall = True
 
-# Write out
-tc2.to_shapefile(current_directory)
-tc2.to_spiderweb(current_directory)
-tc2.make_figures(current_directory)
+    # Forecasting
+    # Uses DeMaria et al. 2009 formulations
+    tc2 = TropicalCycloneEnsemble(name="Idai_forecastng", TropicalCyclone=tc)
+    tc2.dt = 3  # 3 hourly
+    tc2.include_best_track = 0  # not included best-track
+    tc2.tstart = datetime(2019, 3, 9, 0, 0, 0)  # March 5, 2019 at midnight UTC
+    tc2.tend = datetime(2019, 3, 16, 6, 0, 0)  # last timestamp in cyc
+    nm_to_m = float(1.852) * 1000  # nm to m
+    tc2.mean_abs_cte24 = (
+        19.0397 * nm_to_m
+    )  # mean absolute error in cross-track error (CTE) in meter
+    tc2.sc_cte = 1.3253  # auto-regression CTE; typically >1
+    tc2.compute_ensemble(10)
+
+    # Write out
+    tc2.to_shapefile(current_directory)
+    tc2.to_spiderweb(current_directory)
+    tc2.make_figures(current_directory)
+
+    # asserts ?
